0eac26fb2442c785435c0e6f30d71e9ad1b4eb5a
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-10 00:19:17 +0530

    done

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,20 @@
-// import using require
+const Shape = require("./shape");
+
+// export class using module.exports
+
+class Circle extends Shape {
+	constructor() {
+		//first line should be the call to super class constructor with appropriate parameter
+		super();
+	}
+	calculateArea() {
+		console.log("Area of circle with color: " + this.color);
+		return 3.14 * 5 * 5;
+	}
+}
+
+module.exports = Circle; // import using require
 
 // declare class
-
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,22 @@
 // declare class
 
 // export class using module.exports
+
+// declare class
+
+// export class using module.exports
+
+class Shape {
+	constructor(color) {
+		this.color = "red";
+	}
+	drawShape() {
+		console.log("Drawing a Shape");
+	}
+	calculateArea() {
+		console.log("Unknown Shape");
+		return 0;
+	}
+}
+
+module.exports = Shape;
